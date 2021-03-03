c6a2dade3445ddb19821dbb18247d42d4f3c5c02
Jesse Wenger <wengerj@gmail.com>
2021-03-03 22:28:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,5 +23,6 @@ end
 def find_the_cheese(cheese_array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
+  cheese_array.find do |cheese|
+    cheese_types.include? 
 end
